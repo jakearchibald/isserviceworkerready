6fb649816fb7544714fb3e74c9c9ca86dbcd2dcf
Jake Archibald <jaffathecake@gmail.com>
2014-05-27 09:33:53 +0100

    SW test

--- www/static/js/index.js
+++ www/static/js/index.js
@@ -1,1 +1,14 @@
-console.log("NOT SW!");
+window.onmessage = function(event) {
+  console.log(event);
+};
+
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/isserviceworkerready/static/js/sw/all.js', {
+    scope: '/isserviceworkerready/*'
+  }).then(function(val) {
+    console.log("registered!", val);
+    //val.postMessage({hello: "world"});
+  }).catch(function(err) {
+    console.error(err);
+  });
+}
--- www/static/js/sw/index.js
+++ www/static/js/sw/index.js
@@ -1,1 +1,7 @@
-console.log("SW!");
+console.log("This log is from the serviceworker!");
+console.log(this.addEventListener);
+addEventListener('message', function(event) {
+  //console.log("onmessage");
+  //console.log(event);
+  //event.source.postMessage("Reply from SW");
+});
