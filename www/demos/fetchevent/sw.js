50613f8f92f0cdb6d36127adad69af6a4058a75d
Jake Archibald <jaffathecake@gmail.com>
2014-05-28 08:59:25 +0100

    Well that was stupid of me

--- www/demos/fetchevent/sw.js
+++ www/demos/fetchevent/sw.js
@@ -1,8 +1,12 @@
 console.log("SW startup");
 
-this.fetch = function(event) {
+this.onfetch = function(event) {
   console.log("Fetch event", event);
   console.log(".request", event.request);
   console.log(".respondWith", event.respondWith);
   console.log(".default", event.default);
+
+  if (event.respondWith) {
+    event.respondWith(new Response({body: "This is from the SW!"}));
+  }
 };
--- www/demos/installactivate/sw.js
+++ www/demos/installactivate/sw.js
@@ -1,12 +1,22 @@
 console.log("SW startup");
 
-this.install = function(event) {
+this.oninstall = function(event) {
   console.log("Install event", event);
   console.log(".replace", event.replace);
   console.log(".waitUntil", event.waitUntil);
+
+  if (event.waitUntil) {
+    console.log("Testing waitUntil:");
+    event.waitUntil(new Promise(function(resolve) {
+      setTimeout(function() {
+        console.log("This should appear before activate");
+        resolve();
+      }, 3000);
+    }));
+  }
 };
 
-this.activate = function(event) {
+this.onactivate = function(event) {
   console.log("Activate event", event);
   console.log(".waitUntil", event.waitUntil);
 };
