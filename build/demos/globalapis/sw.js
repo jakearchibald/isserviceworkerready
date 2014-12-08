ae4dc50da72b5ff98c92da329feae0e975ebb5ac
Cătălin Mariș <alrraa@gmail.com>
2014-12-08 15:11:45 +0200

    Make Travis automatically update `gh-pages` branch
    Configure Travis to automatically update the content from the
    `gh-pages` branch whenever something is pushed to the `master`
    branch and the tests pass.

--- build/demos/globalapis/sw.js
+++
@@ -1,7 +0,0 @@
-console.log("SW startup");
-console.log("Request", this.Request);
-console.log("Response", this.Response);
-console.log("fetch", this.fetch);
-console.log("Cache", this.Cache);
-console.log("caches", this.caches);
-console.log("getAll", this.getAll);
--- build/demos/install-fail/execution-error/sw.js
+++
@@ -1,6 +0,0 @@
-throw Error("Faillllllll");
-
-self.addEventListener('fetch', function(event) {
-  // we never get here
-  event.respondWith(new Response("If you get this response, there's a bug"));
-});
