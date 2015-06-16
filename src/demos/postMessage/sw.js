81c22232a9051afc40239f20432efd0b0ba461b6
Jake Archibald <jaffathecake@gmail.com>
2015-06-16 14:41:08 +0100

    Big update!

--- src/demos/postMessage/sw.js
+++ src/demos/postMessage/sw.js
@@ -4,13 +4,25 @@ this.onmessage = function(event) {
   console.log("Got message in SW", event.data.text);
   
   if (event.source) {
+    console.log("event.source present");
     event.source.postMessage("Woop!");
   }
   else {
     console.log("No event.source");
+    if (event.data.port) {
+      event.data.port.postMessage("Woop!");
+    }
   }
 
-  if (event.data.port) {
-    event.data.port.postMessage("Woop!");
+  if (self.clients) {
+    console.log("Attempting postMessage via clients API");
+    clients.matchAll().then(function(clients) {
+      for (var client of clients) {
+        client.postMessage("Whoop! (via client api)");
+      }
+    });
+  }
+  else {
+    console.log("No clients API");
   }
 };
