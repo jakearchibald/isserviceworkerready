4b187d8fc145a22a5b9cd94335252472fbc3caf5
Jake Archibald <jaffathecake@gmail.com>
2015-12-08 11:57:55 +0000

    Updating background sync test

--- src/demos/sync/sw.js
+++ src/demos/sync/sw.js
@@ -5,7 +5,3 @@ self.addEventListener('install', function(event) {
 self.addEventListener('sync', function(event) {
   self.registration.showNotification("Sync event fired!");
 });
-
-self.addEventListener('message', function(event) {
-  self.registration.sync.register({tag: 'syncTest'});
-});
--- src/demos/sync/worker.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener('message', function(event) {
-  navigator.serviceWorker.ready.then(function(reg) {
-    return reg.sync.register({tag: 'syncTest'});
-  });
-});
