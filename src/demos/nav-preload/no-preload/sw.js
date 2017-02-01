5ff47c8180f0f22409f043aaf4cf6890332591b9
Jake Archibald <jaffathecake@gmail.com>
2017-02-01 09:52:01 -0800

    Reorging preload demo

--- src/demos/nav-preload/no-preload/sw.js
+++ src/demos/nav-preload/no-preload/sw.js
@@ -5,15 +5,7 @@ while (Date.now() - start < 500);
 addEventListener('install', event => {
   event.waitUntil(async function() {
     const cache = await caches.open('nav-preload-demo-v1');
-    await cache.add('styles.css');
-  }());
-});
-
-addEventListener('activate', event => {
-  event.waitUntil(async function() {
-    if (self.registration.navigationPreload) {
-      await self.registration.navigationPreload.enable();
-    }
+    await cache.add('../styles.css');
   }());
 });
 
@@ -22,12 +14,7 @@ addEventListener('fetch', event => {
     // Respond from the cache if we can
     const cachedResponse = await caches.match(event.request);
     if (cachedResponse) return cachedResponse;
-
-    // Use the preloaded response, if it's there
-    const response = await event.preloadResponse;
-    if (response) return response;
-    
-    // Else try the network.
+    // Else try the network
     return fetch(event.request);
   }());
 });
--- src/demos/nav-preload/script.js
+++
@@ -1,8 +0,0 @@
-navigator.serviceWorker.register('sw.js');
-
-// Log performance details:
-window.addEventListener('load', () => {
-  for (const key of ['fetchStart', 'connectStart', 'connectEnd', 'requestStart', 'responseStart', 'responseEnd', 'domComplete']) {
-    console.log(key, performance.timing[key] - performance.timing.navigationStart);
-  }
-});
